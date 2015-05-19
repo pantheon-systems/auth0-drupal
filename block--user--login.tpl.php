2d4ae2d237e5d058c733fec05dead2686691d672
German Lena <german.lena@gmail.com>
2015-05-19 09:54:40 -0300

    Updated to use Lock7

--- block--user--login.tpl.php
+++ block--user--login.tpl.php
@@ -1,39 +1,31 @@
 <?php global $base_root; ?>
 <?php
-$params = array(
-    'domain' => filter_var(variable_get("auth0_domain"), FILTER_SANITIZE_STRING),
-    'chrome' => true,
-    'clientID' => filter_var(variable_get("auth0_client_id"), FILTER_SANITIZE_STRING),
-    'callbackURL' => filter_var("$base_root/auth0/callback", FILTER_VALIDATE_URL),
-    'state' => filter_var(uniqid(), FILTER_SANITIZE_STRING),
-    'showSignup' => filter_var(variable_get("auth0_allow_signup"), FILTER_VALIDATE_BOOLEAN)
-);
+
+$domain = filter_var(variable_get("auth0_domain"), FILTER_SANITIZE_STRING);
+$client_id = filter_var(variable_get("auth0_client_id"), FILTER_SANITIZE_STRING);
 
 $params = array(
-    'domain' => filter_var(variable_get("auth0_domain"), FILTER_SANITIZE_STRING),
-    'chrome' => true,
-    'clientID' => filter_var(variable_get("auth0_client_id"), FILTER_SANITIZE_STRING),
     'callbackURL' => filter_var("$base_root/auth0/callback", FILTER_VALIDATE_URL),
-    'state' => filter_var(variable_get('$state'), FILTER_SANITIZE_STRING),
-    'showSignup' => filter_var(variable_get("auth0_allow_signup"), FILTER_VALIDATE_BOOLEAN)
+    'authParams' => array(
+        'state' => filter_var(variable_get('$state'), FILTER_SANITIZE_STRING),
+    ),
+    'disableSignupAction' => !filter_var(variable_get("auth0_allow_signup"), FILTER_VALIDATE_BOOLEAN),
+    'container' => 'auth0-login-form'
 );
 
 ?>
-
-<script id="auth0" src="<?php echo filter_var(variable_get('auth0_widget_cdn'), FILTER_VALIDATE_URL);?>"></script>
-<script type="text/javascript">
-
-    var widget = new Auth0Widget(<?php echo json_encode(array_filter($params)); ?>);
-
-    widget.signin({
-        container:      'auth0-login-form',
-        scope: 'openid profile'
-    });
-
-</script>
 
 <style type="text/css">
 <?php echo variable_get("auth0_login_css")?>
 </style>
 
 <div id="auth0-login-form" style=" min-height: 440px;"></div>
+
+<script id="auth0" src="<?php echo filter_var(variable_get('auth0_widget_cdn'), FILTER_VALIDATE_URL);?>"></script>
+<script type="text/javascript">
+
+    var lock = new Auth0Lock('<?php echo $client_id; ?>', '<?php echo $domain; ?>');
+
+    lock.show(<?php echo json_encode($params); ?>);
+
+</script>
